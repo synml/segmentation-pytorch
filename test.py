a70f198b28b03645ba4c007f5540cfecf8e48249
Lulin <kistssy+dev@gmail.com>
2020-10-19 21:16:05 +0900

    train, test 코드 작성 중
    1. 평가지표를 계산하는 코드 필요

---
+++ test.py
@@ -0,0 +1,98 @@
+import configparser
+import csv
+import os
+import time
+
+import torch.utils.data
+import torch.nn.functional as F
+import torchvision
+import tqdm
+
+import model.unet
+
+
+def evaluate(model, testloader, device):
+    model.eval()
+
+    # Evaluate
+    total_loss = 0
+    entire_time = 0
+    for images, masks in tqdm.tqdm(testloader, desc='Batch', leave=False):
+        # 이미지와 정답 정보를 GPU로 복사
+        images = images.to(device)
+        masks = masks.to(device)
+
+        # 순전파
+        start_time = time.time()
+        with torch.no_grad():
+            masks_pred = model(images)
+        entire_time += time.time() - start_time
+
+        # validation loss를 모두 합침
+        total_loss += F.cross_entropy(masks_pred, masks).item()
+
+    # 평균 validation loss 계산
+    val_loss = total_loss / testloader.__len__()
+
+    # 추론 시간과 fps를 계산
+    inference_time = entire_time / testloader.__len__()
+    fps = 1 / inference_time
+
+    # 추론 시간을 miliseconds 단위로 설정
+    inference_time *= 1000
+
+    return val_loss, inference_time, fps
+
+
+if __name__ == '__main__':
+    parser = configparser.ConfigParser()
+    parser.read('config.ini', encoding='utf-8')
+    config = {
+        'batch_size': parser.getint('U-Net', 'batch_size'),
+        'epoch': parser.getint('U-Net', 'epoch'),
+        'image_size': parser.getint('U-Net', 'image_size'),
+        'lr': parser.getfloat('U-Net', 'lr'),
+        'num_workers': parser.getint('U-Net', 'num_workers'),
+        'pretrained_weights': parser.getint('U-Net', 'pretrained_weights')
+    }
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # 데이터셋 설정
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(config['image_size']),
+        torchvision.transforms.ToTensor()
+    ])
+    testset = torchvision.datasets.Cityscapes(root='../../data/cityscapes',
+                                              split='val',
+                                              mode='fine',
+                                              target_type='semantic',
+                                              transform=transform,
+                                              target_transform=torchvision.transforms.ToTensor())
+    testloader = torch.utils.data.DataLoader(testset,
+                                             batch_size=config['batch_size'],
+                                             shuffle=True,
+                                             num_workers=config['num_workers'],
+                                             pin_memory=True)
+
+    # 모델 설정
+    model = model.unet.UNet(3, 19).to(device)
+    model.load_state_dict(torch.load(config['pretrained_weights']))
+
+    # 모델 평가
+    val_loss, inference_time, fps = evaluate(model, testloader, device)
+
+    # Validation loss, Inference time, FPS 출력
+    print('Validation loss: {:.4f}'.format(val_loss))
+    print('Inference time (ms): {:.02f}'.format(inference_time))
+    print('FPS: {:.02f}'.format(fps))
+
+    # Validation loss, Inference time, FPS를 csv 파일로 저장
+    os.makedirs('csv', exist_ok=True)
+    now = time.strftime('%y%m%d_%H%M%S', time.localtime(time.time()))
+    with open('csv/test{}.csv'.format(now), mode='w') as f:
+        writer = csv.writer(f, delimiter=',', lineterminator='\n')
+        writer.writerow(['Validation loss', val_loss])
+        writer.writerow(['Inference time (ms)', inference_time])
+        writer.writerow(['FPS', fps])
+    print('평가 결과를 csv 파일로 저장했습니다.')
--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import torch.utils.tensorboard
 import tqdm
 
 import model.unet
+import test
 
 if __name__ == '__main__':
     parser = configparser.ConfigParser()
@@ -39,6 +40,17 @@ if __name__ == '__main__':
                                               shuffle=True,
                                               num_workers=config['num_workers'],
                                               pin_memory=True)
+    testset = torchvision.datasets.Cityscapes(root='../../data/cityscapes',
+                                              split='val',
+                                              mode='fine',
+                                              target_type='semantic',
+                                              transform=transform,
+                                              target_transform=torchvision.transforms.ToTensor())
+    testloader = torch.utils.data.DataLoader(testset,
+                                             batch_size=config['batch_size'],
+                                             shuffle=True,
+                                             num_workers=config['num_workers'],
+                                             pin_memory=True)
 
     # 모델 설정
     model = model.unet.UNet(3, 19)
@@ -62,7 +74,7 @@ if __name__ == '__main__':
         model.train()
 
         # 1 epoch의 각 배치에서 처리하는 코드
-        for batch_idx, (_, images, masks) in enumerate(tqdm.tqdm(trainloader, desc='Batch', leave=False)):
+        for batch_idx, (images, masks) in enumerate(tqdm.tqdm(trainloader, desc='Batch', leave=False)):
             step = len(trainloader) * epoch + batch_idx
 
             # 이미지와 정답 정보를 GPU로 복사
@@ -72,20 +84,26 @@ if __name__ == '__main__':
             # 변화도(Gradient) 매개변수를 0으로 만들기
             optimizer.zero_grad()
 
-            # 순전파 + 역전파 + 최적화를 한 후
+            # 순전파 + 역전파 + 최적화
             masks_pred = model(images)
             loss = criterion(masks_pred, masks)
             loss.backward()
             optimizer.step()
 
-            # 총 손실값 출력
+            # 손실값 출력
             log_loss.set_description_str('Loss: {:.4f}'.format(loss.item()))
 
             # Tensorboard에 loss 기록
-            writer.add_scalar('Loss', loss.item(), step)
+            writer.add_scalar('Train loss', loss.item(), step)
+
+        # 모델을 평가
+        val_loss, _, _ = test.evaluate(model, testloader, device)
+
+        # Tensorboard에 평가 결과 기록
+        writer.add_scalar('Val loss', val_loss, epoch)
 
         # lr scheduler의 step을 진행
-        scheduler.step()
+        scheduler.step(val_loss)
 
         # checkpoint file 저장
         now = time.strftime('%y%m%d_%H%M%S', time.localtime(time.time()))
