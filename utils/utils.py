260a51674deb77586ad9ae3bc5d9cf982e9e15f3
Lulin <kistssy+dev@gmail.com>
2020-10-22 15:51:16 +0900

    utils: remove squeeze function
    사용하면 안됨

--- utils/utils.py
+++ utils/utils.py
@@ -13,8 +13,8 @@ def make_plt_subplot(nrows: int, ncols: int, index: int, title: str, image):
 def show_dataset(images, masks):
     # make subplot
     to_pil_image = torchvision.transforms.ToPILImage()
-    make_plt_subplot(1, 2, 1, 'Input image', to_pil_image(images.cpu().squeeze(0)))
-    make_plt_subplot(1, 2, 2, 'Groundtruth', to_pil_image(masks.cpu().squeeze(0)))
+    make_plt_subplot(1, 2, 1, 'Input image', to_pil_image(images.cpu()))
+    make_plt_subplot(1, 2, 2, 'Groundtruth', to_pil_image(masks.cpu()))
 
     # show plot
     plt.show()
