4cbb4794186d8efe77a13c340d89bd6a0794b174
Lulin <kistssy+dev@gmail.com>
2020-11-11 11:35:44 +0900

    model: Refactor to simplify code

--- model/unet.py
+++ model/unet.py
@@ -29,57 +29,39 @@ class UNet(nn.Module):
     def __init__(self, num_channels, num_classes):
         super(UNet, self).__init__()
 
-        self.down1 = double_conv(num_channels, 64)
-        self.down2 = double_conv(64, 128)
-        self.down3 = double_conv(128, 256)
-        self.down4 = double_conv(256, 512)
-        self.bottom = double_conv(512, 1024)
+        self.encode1 = double_conv(num_channels, 64)
+        self.encode2 = double_conv(64, 128)
+        self.encode3 = double_conv(128, 256)
+        self.encode4 = double_conv(256, 512)
+        self.encode5 = double_conv(512, 1024)
 
-        self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.upconv4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
 
-        self.up4 = double_conv(1024, 512)
-        self.up3 = double_conv(512, 256)
-        self.up2 = double_conv(256, 128)
-        self.up1 = double_conv(128, 64)
+        self.decode4 = double_conv(1024, 512)
+        self.decode3 = double_conv(512, 256)
+        self.decode2 = double_conv(256, 128)
+        self.decode1 = double_conv(128, 64)
 
         self.classifier = nn.Conv2d(64, num_classes, kernel_size=1)
 
     def forward(self, x):
-        down1 = self.down1(x)
+        # Encoder
+        encode1 = self.encode1(x)
+        encode2 = self.encode2(F.max_pool2d(encode1, 2))
+        encode3 = self.encode3(F.max_pool2d(encode2, 2))
+        encode4 = self.encode4(F.max_pool2d(encode3, 2))
+        encode_end = self.encode5(F.max_pool2d(encode4, 2))
 
-        x = F.max_pool2d(down1, 2)
-        down2 = self.down2(x)
+        # Decoder
+        x = self.decode4(torch.cat([self.upconv4(encode_end), encode4], dim=1))
+        x = self.decode3(torch.cat([self.upconv3(x), encode3], dim=1))
+        x = self.decode2(torch.cat([self.upconv2(x), encode2], dim=1))
+        x = self.decode1(torch.cat([self.upconv1(x), encode1], dim=1))
 
-        x = F.max_pool2d(down2, 2)
-        down3 = self.down3(x)
-
-        x = F.max_pool2d(down3, 2)
-        down4 = self.down4(x)
-
-        x = F.max_pool2d(down4, 2)
-        x = self.bottom(x)
-
-        # --------------------------------------
-
-        x = self.upconv1(x)
-        x = torch.cat([x, down4], dim=1)
-        x = self.up4(x)
-
-        x = self.upconv2(x)
-        x = torch.cat([x, down3], dim=1)
-        x = self.up3(x)
-
-        x = self.upconv3(x)
-        x = torch.cat([x, down2], dim=1)
-        x = self.up2(x)
-
-        x = self.upconv4(x)
-        x = torch.cat([x, down1], dim=1)
-        x = self.up1(x)
-
+        # Classifier
         x = self.classifier(x)
         return x
 
@@ -89,6 +71,7 @@ if __name__ == '__main__':
 
     model = UNet(3, 20).to(device)
     model.apply(utils.utils.init_weights)
+    model.eval()
 
     input_image = torch.rand(1, 3, 256, 256).to(device)
     out = model(input_image)
