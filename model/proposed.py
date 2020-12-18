3f1737cad5ff912f675867d9c4e41c6643695b08
Lulin <kistssy+dev@gmail.com>
2020-12-18 16:13:05 +0900

    proposed: resnet50의 사전학습된 residual block 사용

--- model/proposed.py
+++ model/proposed.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.tensorboard
+import torchvision
 import torchsummary
 
 
@@ -92,30 +93,35 @@ class ResidualBlock(nn.Module):
 class Proposed(nn.Module):
     def __init__(self, num_channels, num_classes):
         super(Proposed, self).__init__()
+        resnet50 = torchvision.models.resnet50(pretrained=True)
 
         self.encode1 = self.double_conv(num_channels, 64)
-        self.encode2 = self.make_layer(64, 128, 3)
-        self.encode3 = self.make_layer(128, 256, 4)
-        self.encode4 = self.make_layer(256, 512, 6)
-        self.encode5 = self.make_layer(512, 1024, 3)
+        self.encode2 = resnet50.layer1  # 256
+        self.encode3 = resnet50.layer2  # 512
+        self.encode4 = resnet50.layer3  # 1024
+        self.encode_end = resnet50.layer4  # 2048
 
-        self.decode4 = self.double_conv(1024, 512)
-        self.decode3 = self.double_conv(512, 256)
-        self.decode2 = self.double_conv(256, 128)
-        self.decode1 = self.double_conv(128, 64)
+        self.upconv4 = nn.ConvTranspose2d(2048, 1024, kernel_size=2, stride=2)
+        self.decode4 = self.double_conv(2048, 1024)
 
-        self.upconv4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.upconv3 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.decode3 = self.double_conv(1024, 512)
+
+        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.decode2 = self.double_conv(512, 256)
+
+        self.upconv1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.decode1 = self.double_conv(128 + 64, 64)
 
         self.classifier = nn.Conv2d(64, num_classes, kernel_size=1)
 
     def double_conv(self, in_channels, out_channels):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
@@ -131,9 +137,9 @@ class Proposed(nn.Module):
         # Encoder
         encode1 = self.encode1(x)
         encode2 = self.encode2(F.max_pool2d(encode1, 2))
-        encode3 = self.encode3(F.max_pool2d(encode2, 2))
-        encode4 = self.encode4(F.max_pool2d(encode3, 2))
-        encode_end = self.encode5(F.max_pool2d(encode4, 2))
+        encode3 = self.encode3(encode2)
+        encode4 = self.encode4(encode3)
+        encode_end = self.encode_end(encode4)
 
         # Decoder
         out = self.decode4(torch.cat([self.upconv4(encode_end), encode4], dim=1))
@@ -149,7 +155,7 @@ class Proposed(nn.Module):
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model = Proposed(3, 8).to(device)
+    model = Proposed(3, 20).to(device)
     model.eval()
 
     torchsummary.torchsummary.summary(model, (3, 256, 512))
--- model/unet.py
+++ model/unet.py
@@ -57,7 +57,7 @@ class UNet(nn.Module):
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model = UNet(3, 8).to(device)
+    model = UNet(3, 20).to(device)
     model.eval()
 
     torchsummary.torchsummary.summary(model, (3, 256, 512))
