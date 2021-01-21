0f0ff86c4fa7cf37671a22f91369c90e194f0035
Lulin <kistssy@gmail.com>
2021-01-21 16:07:51 +0900

    utils: Change print format of get_model

--- utils.py
+++ utils.py
@@ -54,7 +54,7 @@ def get_model(model_name: str, num_classes: int, pretrained: str = None) -> torc
         if os.path.exists(pretrained):
             model.load_state_dict(torch.load(pretrained))
         else:
-            print('FileNotFound: ' + model_name + ' pretrained_weights')
+            print('FileNotFound: pretrained_weights (' + model_name + ')')
     return model
 
 
