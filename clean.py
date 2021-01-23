5b86d2546d97f6ea1996121512c1d60e0bb7913b
Lulin <kistssy@gmail.com>
2021-01-24 00:13:49 +0900

    clean: Add removing directory

--- clean.py
+++ clean.py
@@ -1,6 +1,7 @@
 import shutil
 
 shutil.rmtree('demo', ignore_errors=True)
+shutil.rmtree('feature_maps', ignore_errors=True)
 shutil.rmtree('result', ignore_errors=True)
 shutil.rmtree('runs', ignore_errors=True)
 shutil.rmtree('weights', ignore_errors=True)
