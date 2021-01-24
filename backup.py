5f62eb2d271c76d1f99a73e671e30858927e9684
Lulin <kistssy+dev@gmail.com>
2021-01-25 04:27:16 +0900

    backup: Fix critical issues

--- backup.py
+++ backup.py
@@ -3,11 +3,20 @@ import os
 import shutil
 
 
-def move_directory(src: str, dst):
-    try:
+def move_directory(src: str, dst: str):
+    if os.path.exists(src):
         shutil.move(src, dst)
-    except FileNotFoundError:
-        pass
+    else:
+        print('FileNotFound: ' + src)
+
+
+def move_files(src: str, dst: str):
+    if os.path.exists(src):
+        for file in glob.glob(src + '/*'):
+            shutil.move(file, dst)
+        os.rmdir(src)
+    else:
+        print('FileNotFound: ' + src)
 
 
 # Set backup directory
@@ -15,16 +24,11 @@ backup_dir = input('Directory name: ')
 if backup_dir == '':
     backup_dir = 'rename'
 backup_dir = os.path.join('backup', backup_dir)
-try:
-    os.makedirs(backup_dir)
-except FileExistsError as e:
-    print(e)
+os.makedirs(backup_dir)
 
 # Backup
 move_directory('demo', backup_dir)
 move_directory('feature_maps', backup_dir)
 move_directory('runs', backup_dir)
-for file in glob.glob('result/*'):
-    shutil.move(file, backup_dir)
-for file in glob.glob('weights/*'):
-    shutil.move(file, backup_dir)
+move_files('result', backup_dir)
+move_files('weights', backup_dir)
