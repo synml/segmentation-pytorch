b619a6568e4d73a48af89b2694122e9d185b4fbe
Lulin <kistssy+dev@gmail.com>
2021-01-26 01:42:42 +0900

    Change names to image and target

--- utils.py
+++ utils.py
@@ -186,8 +186,8 @@ class DataAugmentation:
         return self.__class__.__name__ + '()'
 
 
-# 데이터셋 불러오는 코드 검증
-def show_dataset(images: torch.Tensor, masks: torch.Tensor):
+# 데이터셋 불러오는 코드 검증 (Shape: [batch, channel, height, width])
+def show_dataset(image: torch.Tensor, target: torch.Tensor):
     def make_plt_subplot(nrows: int, ncols: int, index: int, title: str, image):
         plt.subplot(nrows, ncols, index)
         plt.title(title)
@@ -197,8 +197,8 @@ def show_dataset(images: torch.Tensor, masks: torch.Tensor):
 
     to_pil_image = torchvision.transforms.ToPILImage()
 
-    assert images.shape[0] == masks.shape[0]
-    for i in range(images.shape[0]):
-        make_plt_subplot(1, 2, 1, 'Input image', to_pil_image(images[i].squeeze().cpu()))
-        make_plt_subplot(1, 2, 2, 'Groundtruth', to_pil_image(masks[i].cpu()))
+    assert image.shape[0] == target.shape[0]
+    for i in range(image.shape[0]):
+        make_plt_subplot(1, 2, 1, 'Input image', to_pil_image(image[i].squeeze().cpu()))
+        make_plt_subplot(1, 2, 2, 'Groundtruth', to_pil_image(target[i].cpu()))
         plt.show()
