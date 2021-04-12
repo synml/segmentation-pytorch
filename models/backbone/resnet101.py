26954036a56f17ae99463915a9ee765294603bc9
Clova <kistssy@gmail.com>
2021-04-12 19:58:31 +0900

    model: Implement deeplabv3plus

---
+++ models/backbone/resnet101.py
@@ -0,0 +1,50 @@
+import torch
+import torch.utils.tensorboard
+import torchvision
+import torchsummary
+
+
+class ResNet101(torchvision.models.resnet.ResNet):
+    def __init__(self, output_stride: int) -> None:
+        if output_stride == 16:
+            replace_stride_with_dilation = [False, False, True]
+        elif output_stride == 8:
+            replace_stride_with_dilation = [False, True, True]
+        else:
+            raise NotImplementedError('Wrong output_stride.')
+
+        super(ResNet101, self).__init__(torchvision.models.resnet.Bottleneck, [3, 4, 23, 3],
+                                        replace_stride_with_dilation=replace_stride_with_dilation)
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        low_level_feature = x
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x, low_level_feature
+
+
+def load_resnet101(output_stride: int, pretrained: bool):
+    model = ResNet101(output_stride)
+    if pretrained:
+        state_dict = torch.hub.load_state_dict_from_url(torchvision.models.resnet.model_urls['resnet101'])
+        model.load_state_dict(state_dict)
+    return model
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = load_resnet101(output_stride=8, pretrained=True).to(device)
+    model.eval()
+
+    torchsummary.torchsummary.summary(model, (3, 256, 512))
+
+    writer = torch.utils.tensorboard.SummaryWriter('../runs')
+    writer.add_graph(model, torch.rand(1, 3, 256, 512).to(device))
+    writer.close()
---
+++ models/deeplabv3plus.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.tensorboard
+import torchvision
+import torchsummary
+
+import models.backbone.resnet101
+import models.backbone.xception
+
+
+class DeepLabv3plus(nn.Module):
+    def __init__(self, backbone: str, output_stride: int, num_classes: int) -> None:
+        super(DeepLabv3plus, self).__init__()
+        # Backbone
+        if backbone == 'resnet101':
+            self.backbone = models.backbone.resnet101.load_resnet101(output_stride, pretrained=True)
+        elif backbone == 'xception':
+            self.backbone = models.backbone.xception.ModifiedAlignedXception(output_stride, pretrained=True)
+        else:
+            raise NotImplementedError('Wrong backbone.')
+
+        # ASPP
+        if output_stride == 16:
+            aspp_atrous_rates = [6, 12, 18]
+        elif output_stride == 8:
+            aspp_atrous_rates = [12, 24, 36]
+        else:
+            raise NotImplementedError('Wrong output_stride.')
+        self.aspp = torchvision.models.segmentation.deeplabv3.ASPP(2048, aspp_atrous_rates, 256)
+
+        # Decoder
+        self.decoder = Decoder(backbone, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        size = x.size()[2:]
+
+        x, low_level_feature = self.backbone(x)
+        x = self.aspp(x)
+        x = self.decoder(x, low_level_feature)
+        x = F.interpolate(x, size=size, mode='bilinear', align_corners=True)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, backbone: str, num_classes: int) -> None:
+        super(Decoder, self).__init__()
+        if backbone == 'resnet101':
+            in_channels = 256
+        elif backbone == 'xception':
+            in_channels = 128
+        else:
+            raise NotImplementedError('Wrong backbone.')
+
+        low_level_feature_channels = 48
+        self.compress_low_level_feature = nn.Sequential(
+            nn.Conv2d(in_channels, low_level_feature_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(low_level_feature_channels),
+            nn.ReLU(inplace=True)
+        )
+        self.decode1 = self.make_decoder(in_channels + low_level_feature_channels, 256)
+        self.classifier = nn.Conv2d(256, num_classes, kernel_size=1)
+
+    def forward(self, x: torch.Tensor, low_level_feature: torch.Tensor) -> torch.Tensor:
+        low_level_feature = self.compress_low_level_feature(low_level_feature)
+
+        x = F.interpolate(x, size=low_level_feature.size()[2:], mode='bilinear', align_corners=False)
+        x = torch.cat((x, low_level_feature), dim=1)
+        x = self.decode1(x)
+        x = self.classifier(x)
+        return x
+
+    def make_decoder(self, in_channels: int, out_channels: int) -> nn.Sequential:
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = DeepLabv3plus('resnet101', output_stride=8, num_classes=20).to(device)
+    model.eval()
+
+    torchsummary.torchsummary.summary(model, (3, 400, 800))
+
+    writer = torch.utils.tensorboard.SummaryWriter('../runs')
+    writer.add_graph(model, torch.rand(1, 3, 400, 800).to(device))
+    writer.close()
