4f5ea735c55040e5a2a18c695160175f240383a1
Clova <kistssy@gmail.com>
2021-04-15 15:35:19 +0900

    Change debug code

--- models/backbone/resnet101.py
+++ models/backbone/resnet101.py
@@ -47,7 +47,7 @@ def load_resnet101(output_stride: int) -> ResNet101:
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = load_resnet101(output_stride=8).to(device)
+    model = load_resnet101(output_stride=16).to(device)
     model.eval()
 
     torchsummary.torchsummary.summary(model, (3, 400, 800))
