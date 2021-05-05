eaa0e0f997686b9f6e6866e30686cb89d2c0ac4e
Clova <kistssy@gmail.com>
2021-05-06 03:09:48 +0900

    demo 재구성

--- demo.py
+++ demo.py
@@ -8,41 +8,41 @@ import tqdm
 import utils
 
 if __name__ == '__main__':
-    # 0. Load config
-    config = utils.load_config()
-    print('Activated model: {}'.format(config['model']))
+    # 0. Load cfg and create components builder
+    cfg = utils.builder.load_cfg('cfg.yaml')
+    builder = utils.builder.Builder(cfg)
 
     # 1. Dataset
-    dataset = utils.Cityscapes(config)
-    _, _, testset, testloader = dataset.set_cityscapes()
+    dataset_impl, valset, valloader = builder.build_dataset('val')
 
     # 2. Model
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = utils.get_model(config, pretrained=True).to(device)
-    model.eval()
+    model = builder.build_model(pretrained=True).to(device)
+    model_name = cfg['model']['name']
+    print(f'Activated model: {model_name}')
 
     # 이미지 이름 불러오기
     image_names = []
-    for image_path in testset.images:
+    for image_path in valset.images:
         image_name = image_path.replace('\\', '/').split('/')[-1]
         image_names.append(image_name)
 
     # label colormap 설정
-    cmap = matplotlib.colors.ListedColormap(dataset.get_cityscapes_colormap())
+    cmap = matplotlib.colors.ListedColormap(dataset_impl.get_cityscapes_colormap())
 
     # 예측 결과 저장
     step = 0
-    result_dir = os.path.join('demo', config['model'].lower())
+    result_dir = os.path.join('demo', model_name.lower())
     groundtruth_dir = os.path.join('demo', 'groundtruth')
     os.makedirs(result_dir, exist_ok=True)
     os.makedirs(groundtruth_dir, exist_ok=True)
-    for image, target in tqdm.tqdm(testloader, desc='Demo'):
+    for image, target in tqdm.tqdm(valloader, desc='Demo'):
         # target의 정규화를 해제 (0~1 값을 0~255 값으로 변경) + 채널 차원 제거
         target.mul_(255).squeeze_(dim=1)
         image, target = image.to(device), target.type(torch.LongTensor)
 
         # 예측
-        with torch.cuda.amp.autocast(enabled=config['amp_enabled']):
+        with torch.cuda.amp.autocast(enabled=cfg['model']['amp_enabled']):
             with torch.no_grad():
                 output = model(image)
                 output = torch.argmax(output, dim=1)
