bfd591c2f0ebf2824c366cd0348e5fbf5a5681ab
Clova <kistssy@gmail.com>
2021-05-07 20:56:56 +0900

    datasets: cmap 추가

--- datasets/test.py
+++ datasets/test.py
@@ -4,12 +4,7 @@ import torchvision
 
 
 # 데이터셋 불러오는 코드 검증 (Shape: [batch, channel, height, width])
-def show_dataset(images: torch.Tensor, targets: torch.Tensor):
-    def make_plt_subplot(nrows: int, ncols: int, index: int, title: str, image):
-        plt.subplot(nrows, ncols, index)
-        plt.title(title)
-        plt.imshow(image)
-
+def show_dataset(images: torch.Tensor, targets: torch.Tensor, cmap):
     to_pil_image = torchvision.transforms.ToPILImage()
     plt.rcParams['figure.figsize'] = (17, 6)
     plt.rcParams['figure.autolayout'] = True
@@ -20,6 +15,9 @@ def show_dataset(images: torch.Tensor, targets: torch.Tensor):
 
     assert images.shape[0] == targets.shape[0]
     for i in range(images.shape[0]):
-        make_plt_subplot(1, 2, 1, 'Input image', to_pil_image(images[i].cpu()))
-        make_plt_subplot(1, 2, 2, 'Groundtruth', targets[i].cpu())
+        fig, axs = plt.subplots(1, 2)
+        axs[0].set_title('Input image')
+        axs[0].imshow(to_pil_image(images[i].cpu()))
+        axs[1].set_title('Groundtruth')
+        axs[1].imshow(targets[i].cpu(), cmap=cmap, vmin=0, vmax=cmap.N)
         plt.show()
