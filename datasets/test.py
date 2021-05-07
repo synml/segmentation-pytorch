1788a7d4c40ca37c259b2eb91f170d362d473acd
Clova <kistssy@gmail.com>
2021-05-07 19:25:15 +0900

    datasets: Refactor test.py

--- datasets/test.py
+++ datasets/test.py
@@ -4,7 +4,7 @@ import torchvision
 
 
 # 데이터셋 불러오는 코드 검증 (Shape: [batch, channel, height, width])
-def show_dataset(image: torch.Tensor, target: torch.Tensor):
+def show_dataset(images: torch.Tensor, targets: torch.Tensor):
     def make_plt_subplot(nrows: int, ncols: int, index: int, title: str, image):
         plt.subplot(nrows, ncols, index)
         plt.title(title)
@@ -14,8 +14,8 @@ def show_dataset(image: torch.Tensor, target: torch.Tensor):
 
     to_pil_image = torchvision.transforms.ToPILImage()
 
-    assert image.shape[0] == target.shape[0]
-    for i in range(image.shape[0]):
-        make_plt_subplot(1, 2, 1, 'Input image', to_pil_image(image[i].squeeze().cpu()))
-        make_plt_subplot(1, 2, 2, 'Groundtruth', to_pil_image(target[i].cpu()))
+    assert images.shape == targets.shape
+    for i in range(images.shape[0]):
+        make_plt_subplot(1, 2, 1, 'Input image', to_pil_image(images[i].squeeze().cpu()))
+        make_plt_subplot(1, 2, 2, 'Groundtruth', to_pil_image(targets[i].cpu()))
         plt.show()
