0a6768ae41b66088aa544f2f72083553521445be
Clova <kistssy@gmail.com>
2021-05-12 17:00:44 +0900

    Adam, Dropout 사용

--- models/deeplabv3plus.py
+++ models/deeplabv3plus.py
@@ -69,9 +69,11 @@ class Decoder(nn.Module):
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
+            nn.Dropout(0.1)
         )
 
 
