fd640f296c3b02bc0b46d2619f96b1281232a9f1
Clova <kistssy@gmail.com>
2021-05-12 11:16:25 +0900

    utils: Edit field

--- utils/lr_scheduler.py
+++ utils/lr_scheduler.py
@@ -2,14 +2,14 @@ import torch
 
 
 class PolyLR(torch.optim.lr_scheduler._LRScheduler):
-    def __init__(self, optimizer: torch.optim.Optimizer, max_iter: int, power=0.9, last_epoch=-1, min_lr=1e-6):
+    def __init__(self, optimizer: torch.optim.Optimizer, max_iter: int, power=0.9, min_lr=1e-6, last_epoch=-1):
         assert max_iter != 0
 
+        self.max_iter = max_iter
         self.power = power
-        self.max_iters = max_iter
         self.min_lr = min_lr
         super(PolyLR, self).__init__(optimizer, last_epoch)
 
     def get_lr(self):
-        return [max(base_lr * (1 - self.last_epoch / self.max_iters) ** self.power, self.min_lr)
+        return [max(base_lr * (1 - self.last_epoch / self.max_iter) ** self.power, self.min_lr)
                 for base_lr in self.base_lrs]
