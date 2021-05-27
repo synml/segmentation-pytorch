1531c2f52ee8131919781e1a88240c1e1178d2c8
Clova <kistssy@gmail.com>
2021-05-28 02:11:50 +0900

    utils: Fix path issue

--- utils/checkpoint_extractor.py
+++ utils/checkpoint_extractor.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     checkpoint = torch.load(checkpoint_path)
 
     # Extract pretrained weight
-    torch.save(checkpoint['model_best_miou'], f'{checkpoint_name.split(".")[0]}_best_miou.pth')
-    torch.save(checkpoint['model_best_val_loss'], f'{checkpoint_name.split(".")[0]}_best_val_loss.pth')
+    torch.save(checkpoint['model_best_miou'], f'{checkpoint_path.split("_")[0]}_best_miou.pth')
+    torch.save(checkpoint['model_best_val_loss'], f'{checkpoint_path.split("_")[0]}_best_val_loss.pth')
