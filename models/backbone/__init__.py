feee669cbb5ae2c7117b0dcf36ad5982a0291dd1
Seokyong Shin <kistssy@gmail.com>
2021-08-16 19:30:23 +0900

    Update __init__.py

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -1,2 +1,5 @@
+import models.backbone.efficientnet
+import models.backbone.efficientnet_blocks
+import models.backbone.efficientnet_builder
 import models.backbone.resnet101
 import models.backbone.xception
