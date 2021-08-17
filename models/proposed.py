9c795a1b5c75f44fd85ddf13a6fcba3038dcfff0
clova <kistssy@gmail.com>
2021-08-17 19:28:42 +0900

    models: Remove dropout

--- models/proposed.py
+++ models/proposed.py
@@ -122,7 +122,6 @@ class Decoder(nn.Module):
         return nn.Sequential(
             models.modules.conv.SeparableConv2d(in_channels, mid_channels, kernel_size=3,
                                                 stride=1, padding=1, bias=False),
-            nn.Dropout2d(0.2),
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
             models.modules.conv.SeparableConv2d(mid_channels, out_channels, kernel_size=3,
