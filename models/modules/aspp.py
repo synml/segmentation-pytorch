b4592040cc855a28c231eb4f41cdce1fe0dfc74b
clova <kistssy@gmail.com>
2021-10-05 16:23:39 +0900

    modules: Add channel attention to aspp

--- models/modules/aspp.py
+++ models/modules/aspp.py
@@ -36,7 +36,9 @@ class ASPPwDSConv(nn.Module):
         modules.append(nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU()))
+            nn.ReLU(),
+            models.modules.attention.ChannelAttention(in_channels)
+        ))
 
         rates = tuple(atrous_rates)
         for rate in rates:
