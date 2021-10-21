041a39807aef2c55640a9c8bb050871e351743a3
clova <kistssy@gmail.com>
2021-10-21 18:49:04 +0900

    proposed: Fix model debug code

--- models/proposed.py
+++ models/proposed.py
@@ -129,5 +129,5 @@ class Decoder(nn.Module):
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = Proposed(num_classes=19, aux_loss=False).to(device)
+    model = Proposed(num_classes=19).to(device)
     models.test.test_model(model, (1, 3, 1024, 2048), '../runs')
