5e012dd945c747d473ba37901141068db20fda8b
clova <kistssy@gmail.com>
2021-11-16 00:55:12 +0900

    hooks: Delete detach()

--- utils/hooks.py
+++ utils/hooks.py
@@ -1,10 +1,10 @@
 def get_feature_maps(feature_maps: list):
     def hook(module, input, output):
-        feature_maps.append(output.detach())
+        feature_maps.append(output)
     return hook
 
 
 def get_feature_maps_with_name(feature_maps: dict, name: str):
     def hook(module, input, output):
-        feature_maps[name] = output.detach()
+        feature_maps[name] = output
     return hook
