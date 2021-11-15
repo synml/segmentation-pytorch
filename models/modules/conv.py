366d4665dd6126b234510ec809bf5408e34b4128
clova <kistssy@gmail.com>
2021-11-15 11:25:16 +0900

    Revert "conv: Add activation to separable convolution"
    This reverts commit 44abec556b982706a44f063f929accbc8f9bf36a.

--- models/modules/conv.py
+++ models/modules/conv.py
@@ -3,15 +3,19 @@ import torch.nn as nn
 
 class SeparableConv2d(nn.Sequential):
     def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride=1, padding=0, dilation=1,
-                 activation=nn.ReLU(), channel_attention: nn.Module = None):
+                 activation: nn.Module = None, channel_attention: nn.Module = None):
         modules = [
             nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, dilation, in_channels, bias=False),
             nn.BatchNorm2d(in_channels),
-            activation,
+            # activation
             # channel_attention
             nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
             nn.BatchNorm2d(out_channels)
         ]
+        if activation is not None:
+            modules.insert(2, activation)
         if channel_attention is not None:
+            if activation is None:
+                raise ValueError('Use activation and channel attention together.')
             modules.insert(3, channel_attention)
         super(SeparableConv2d, self).__init__(*modules)
