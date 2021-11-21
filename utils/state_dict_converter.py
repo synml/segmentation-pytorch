bf7a5b60aba0103f4d34aebdb551eb62e83f8077
clova <kistssy@gmail.com>
2021-11-22 00:47:12 +0900

    utils: Fix state_dict_converter

--- utils/state_dict_converter.py
+++ utils/state_dict_converter.py
@@ -2,7 +2,7 @@ def convert_ddp_state_dict(state_dict: dict):
     new_state_dict = {}
     for key, value in state_dict.items():
         if key.startswith('module.'):
-            new_key = key.replace('module.', '')
+            new_key = key.removeprefix('module.')
             new_state_dict[new_key] = value
         else:
             new_state_dict[key] = value
